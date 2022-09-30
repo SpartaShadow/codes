bef45921a5209367deef9c2864eef342916d1ce0
SpartaShadow <ayanaliansari99@gmail.com>
2022-09-30 19:03:31 +0530

    task 5

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.dir(document);
+// GETELEMENTSBYTAGNAME //
+var li= document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontweight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+for(var i= 0; i < li.length; i ++){
+li[i].style.backgroundColor = '#f4f4f4';
+}
