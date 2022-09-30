1b26f2bda133036a5acaa9d021eccfa8e5e93ae9
SpartaShadow <ayanaliansari99@gmail.com>
2022-09-30 19:25:46 +0530

    task 6

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-// GETELEMENTSBYTAGNAME //
-var li= document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent = 'Hello 2';
-li[1].style.fontweight = 'bold';
-li[1].style.backgroundColor = 'yellow';
-// Gives error
-//items.style.backgroundColor = '#f4f4f4';
-for(var i= 0; i < li.length; i ++){
-li[i].style.backgroundColor = '#f4f4f4';
+// QUERYSELECTOR //
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+var input = document.querySelector('input');
+input.value = 'Hello World'
+var submit =document.querySelector('input[type="submit"]');
+submit.value="SEND";
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'green';
+
+// QUERYSELECTORALL //
+var titles= document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'Hello';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even= document.querySelectorAll('li:nth-child(even)');
+for(var i= 0; i < odd.length; i ++){
+odd[i].style.backgroundColor = '#f4f4f4';
+even[i].style.backgroundColor = '#ccc';
 }
