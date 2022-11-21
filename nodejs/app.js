55c0eeb27c237d8c630f69a15b2fbefaf8ce3bfb
SpartaShadow <114387187+SpartaShadow@users.noreply.github.com>
2022-11-21 17:11:26 +0530

    clean code

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,40 +1,6 @@
-const http = require('http');
-const fs = require('fs')
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    let temp = '';
-    if(url==='/'){ 
-        fs.readFile("message.txt", {encoding: "utf-8"}, (err,data) => {
-            if(err){
-                console.log(err);
-            }
-            res.write('<html>');
-            res.write('<head><title>Enter message</title><head>' );
-            res.write(`<body> ${data} </body>`)
-            res.write(`<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">SEND</button></form></body>`);
-            res.write('</html>');
-            res.end();
-        })
-        
-    }
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        })
-        return req.on('end', ()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message , err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
+const routes = require("./routes");
 
-            });        
-        })     
-        }
-    
-    });
-    server.listen(4000);
+const server = http.createServer(routes);
+server.listen(4000);
