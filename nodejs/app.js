0b23f82e6d669c5aa240dd08059c8838e4a47dd3
SpartaShadow <114387187+SpartaShadow@users.noreply.github.com>
2022-11-24 20:19:45 +0530

    intro to middleware

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("first middleware!");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/addProduct", (req, res, next) => {
+  res.send(
+    "<form action ='/product' method = 'POST'><label for = 'name'>NAME</label><input type = 'text' name = 'title' id = 'name'><br><label for = 'size'>SIZE</label><input type = 'text' name = 'size' id = 'size'><br><button type = 'submit'>ADD PRODUCT</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("second middleware!");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>hello from express!</h1>");
 });
 
